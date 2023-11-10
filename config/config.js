3381ea6d48f6618410886e852ae77ea4fbabbdeb
dpdls90010002000 <dpdls9001000@gmail.com>
2023-11-09 22:46:04 -0500

    Co-authored-by: BobbyJand10 <BobbyJand10@users.noreply.github.com> Co-authored-by: hw1010 <hyewon6588@users.noreply.github.com>

--- config/config.js
+++ config/config.js
@@ -1,14 +1,8 @@
+
 const config = {
     env: process.env.NODE_ENV || 'development', 
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI || "mongodb+srv://dpdls300:HBek6i66lX7noA3W@yeinan.zy7tzqm.mongodb.net/ShoesLocker"||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' + 
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject' 
-    }
-    export default config
-    
-
-    //user,product,category
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://dpdls300:HBek6i66lX7noA3W@yeinan.zy7tzqm.mongodb.net/ShoesLocker"
+  };
+export default config
