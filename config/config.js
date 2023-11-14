f88fe9ffe337147932077abe253eae06b5aca19b
hw1010 <hyewon6588@naver.com>
2023-11-14 15:39:29 -0500

    config.js update

--- config/config.js
+++ config/config.js
@@ -1,9 +1,7 @@
-
 const config = {
     env: process.env.NODE_ENV || 'development', 
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-<<<<<<< HEAD
     mongoUri: process.env.MONGODB_URI || "mongodb+srv://dpdls300:HBek6i66lX7noA3W@yeinan.zy7tzqm.mongodb.net/ShoesLocker?retryWrites=true&w=majority"||
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' + 
@@ -14,8 +12,3 @@ const config = {
     
 
     //user,product,category
-=======
-    mongoUri: process.env.MONGODB_URI || "mongodb+srv://dpdls300:HBek6i66lX7noA3W@yeinan.zy7tzqm.mongodb.net/ShoesLocker"
-  };
-export default config
->>>>>>> b150a46a373a6c4a36e5daecac61c77329968d4c
