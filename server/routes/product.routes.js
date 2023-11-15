ea9bb55e00bd1e0b6ad7beddce2b7c8da20f887c
Gabriel Castellon <144387589+gcastel5@users.noreply.github.com>
2023-11-14 23:34:19 -0500

    Update product.routes.js

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,13 +1,14 @@
 import express from 'express'
-	import productCtrl from '../controllers/product.controller.js' 
-	const router = express.Router()
-	router.route('/api/products') 
-	.get(productCtrl.list)
-	.post(productCtrl.create)
-	router.route('/api/products/:userId') 
-	.get(productCtrl.read)
-	.put(productCtrl.update) 
-	.delete(productCtrl.remove)
+import productCtrl from '../controllers/product.controller.js' 
+const router = express.Router()
+
+router.route('/api/products') 
+.get(productCtrl.list)
+.post(productCtrl.create)
+router.route('/api/products/:userId') 
+.get(productCtrl.read)
+.put(productCtrl.update) 
+.delete(productCtrl.remove)
 router.param('userId', productCtrl.userByID)
 router.route('/api/products').post(productCtrl.create) 
 router.route('/api/products').get(productCtrl.list)
@@ -16,6 +17,4 @@ router.route('/api/products/:userId').get(productCtrl.read)
 router.route('/api/products/:userId').put(productCtrl.update)
 router.route('/api/products/:userId').delete(productCtrl.remove)
 
-    
-    
-	export default router
+export default router
