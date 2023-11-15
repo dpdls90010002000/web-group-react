bfe6891829717b8854f7fe5f1e87c9b8656a3b52
Gabriel Castellon <144387589+gcastel5@users.noreply.github.com>
2023-11-14 23:33:48 -0500

    Update category.routes.js

--- server/routes/category.routes.js
+++ server/routes/category.routes.js
@@ -1,13 +1,14 @@
 import express from 'express'
-	import categoryCtrl from '../controllers/category.controller.js' 
-	const router = express.Router()
-	router.route('/api/categories') 
-	.get(categoryCtrl.list)
-	.post(categoryCtrl.create)
-	router.route('/api/categories/:userId') 
-	.get(categoryCtrl.read)
-	.put(categoryCtrl.update) 
-	.delete(categoryCtrl.remove)
+import categoryCtrl from '../controllers/category.controller.js' 
+const router = express.Router()
+
+router.route('/api/categories') 
+.get(categoryCtrl.list)
+.post(categoryCtrl.create)
+router.route('/api/categories/:userId') 
+.get(categoryCtrl.read)
+.put(categoryCtrl.update) 
+.delete(categoryCtrl.remove)
 router.param('userId', categoryCtrl.userByID)
 router.route('/api/categories').post(categoryCtrl.create) 
 router.route('/api/categories').get(categoryCtrl.list)
@@ -16,6 +17,4 @@ router.route('/api/categories/:userId').get(categoryCtrl.read)
 router.route('/api/categories/:userId').put(categoryCtrl.update)
 router.route('/api/categories/:userId').delete(categoryCtrl.remove)
 
-    
-    
-	export default router
+export default router
