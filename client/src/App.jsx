c70b688cb629008b6a7fff46c325a1f4b9e6f6e9
gcastel5 <gcastellon70@hotmail.com>
2023-11-26 20:43:24 -0500

    Updated client folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -33,3 +33,65 @@ function App() {
 }
 
 export default App
+*/
+
+
+/*import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import MainRouter from '../MainRouter';
+import theme from '../theme';
+//import { hot } from 'react-hot-loader'
+
+const App = () => {
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <MainRouter />
+      </ThemeProvider>
+    </Router>
+    
+  );
+  
+};
+
+React.useEffect(() => {
+const jssStyles = document.querySelector('#jss-server-side') 
+if (jssStyles) {
+jssStyles.parentNode.removeChild(jssStyles) 
+}
+}, [])
+
+export default App;*/
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import MainRouter from '../MainRouter';
+import theme from '../theme';
+//import { hot } from 'react-hot-loader'
+
+const App = () => {
+  React.useEffect(() => {
+    
+    // Clean up server-side injected JSS styles
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <MainRouter />
+      </ThemeProvider>
+    </Router>
+  );
+};
+
+
+// Wrap the App component with the hot module loader
+ //export default hot(module)(App);
+export default App;
+
+
