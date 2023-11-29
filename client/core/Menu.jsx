b2392d2fe802582a1e02a57a07f72b64157b1fc2
dpdls90010002000 <dpdls9001000@gmail.com>
2023-11-29 00:00:05 -0500

    navigation bar

---
+++ client/core/Menu.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import HomeIcon from '@material-ui/icons/Home'
+import Button from '@material-ui/core/Button'
+import auth from './../auth/auth-helper'
+import {Link, withRouter} from 'react-router-dom'
+import CartIcon from '@material-ui/icons/ShoppingCart'
+import Badge from '@material-ui/core/Badge'
+import cart from './../cart/cart-helper'
+import TextField from '@material-ui/core/TextField'
+import Logo from './../assets/images/unicornbikeImg.jpg'
+
+const isActive = (history, path) => {
+  if (history.location.pathname == path)
+    return {color: '#bef67a'}
+  else
+    return {color: '#ffffff'}
+}
+const isPartActive = (history, path) => {
+  if (history.location.pathname.includes(path))
+    return {color: '#bef67a'}
+  else
+    return {color: '#ffffff'}
+}
+const Menu = withRouter(({history}) => (
+  <AppBar position="static">
+    <Toolbar style={{ height: '90px' }}>
+    <img src={Logo} alt="Logo" style={{ marginRight: '10px', height: '90px' }} />
+
+      <Typography variant="h6" color="inherit">
+        Shoe Locker
+      </Typography>
+      <div>
+        <Link to="/">
+          <IconButton aria-label="Home" style={isActive(history, "/")}>
+            <HomeIcon/>
+          </IconButton>
+        </Link>
+        <Link to="/shops/NA">
+          <Button style={isActive(history, "/shops/all")}>Product</Button>
+        </Link>
+        <Link to="/shops/NA">
+          <Button style={isActive(history, "/shops/all")}>Resell</Button>
+        </Link> 
+        <TextField
+        type="text"
+        label="Search"
+        margin="dense"
+        variant="outlined"
+        style={{
+          marginLeft: '10px',
+          width: '200px',
+          backgroundColor: 'white',
+        }}
+      />
+              <Link to="/shops/NA">
+          <Button style={isActive(history, "/shops/all")}>Search</Button>
+        </Link> 
+      </div>
+      <div style={{'position':'absolute', 'right': '10px'}}><span style={{'float': 'right'}}>
+      {
+        !auth.isAuthenticated() && (<span>
+                  <Link to="/cart">
+          <Button style={isActive(history, "/cart")}>
+            Cart
+            <Badge color="secondary" invisible={false} badgeContent={cart.itemTotal()} style={{'marginLeft': '7px'}}>
+              <CartIcon />
+            </Badge>
+          </Button>
+        </Link>  
+          <Link to="/signup">
+            <Button style={isActive(history, "/signup")}>Sign up
+            </Button>
+          </Link>
+          <Link to="/signin">
+            <Button style={isActive(history, "/signin")}>Sign In
+            </Button>
+          </Link>
+        </span>)
+      }
+      {
+        auth.isAuthenticated() && (<span>
+          {auth.isAuthenticated().user.seller && (<Link to="/seller/shops"><Button style={isPartActive(history, "/seller/")}>My Shops</Button></Link>)}
+          <Link to={"/user/" + auth.isAuthenticated().user._id}>
+            <Button style={isActive(history, "/user/" + auth.isAuthenticated().user._id)}>My Profile</Button>
+          </Link>
+          <Button color="inherit" onClick={() => {
+              auth.clearJWT(() => history.push('/'))
+            }}>Sign out</Button>
+        </span>)
+      }
+      </span></div>
+    </Toolbar>
+  </AppBar>
+))
+
+export default Menu
