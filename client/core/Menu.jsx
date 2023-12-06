6ad0e64324ae3b28145f773a0798d4af6d9c059e
dpdls90010002000 <dpdls9001000@gmail.com>
2023-12-06 17:06:02 -0500

    edit navigation bar

--- client/core/Menu.jsx
+++ client/core/Menu.jsx
@@ -10,7 +10,7 @@ import { Link, withRouter } from 'react-router-dom'
 import CartIcon from '@material-ui/icons/ShoppingCart'
 import Badge from '@material-ui/core/Badge'
 import cart from './../cart/cart-helper'
-import TextField from '@material-ui/core/TextField'
+//import TextField from '@material-ui/core/TextField'
 import Logo from './../assets/images/unicornbikeImg.jpg'
 
 const isActive = (history, path) => {
@@ -27,9 +27,9 @@ const isPartActive = (history, path) => {
 }
 const Menu = withRouter(({ history }) => (
   <AppBar position="static">
-    <Toolbar style={{ height: '90px' }}>
-      <img src={Logo} alt="Logo" style={{ marginRight: '10px', height: '90px' }} />
-      <Typography variant="h6" color="inherit">
+    <Toolbar style={{ height: '130px' }}>
+      <img src={Logo} alt="Logo" style={{ marginRight: '10px', height: '130px' }} />
+      <Typography variant="h4" color="inherit">
         Shoe Locker
       </Typography>
       <div>
@@ -39,12 +39,12 @@ const Menu = withRouter(({ history }) => (
           </IconButton>
         </Link>
         <Link to="/shops/NA">
-          <Button style={isActive(history, "/shops/all")}>Product</Button>
+          <Button style={{ ...isActive(history, "/shops/all"), fontSize: "25px", important: "true", fontWeight: "bold" }}>Product</Button>
         </Link>
         <Link to="/shops/all">
-          <Button style={isActive(history, "/shops/all")}>Resell</Button>
+          <Button style={{ ...isActive(history, "/shops/all"), fontSize: "25px", important: "true", fontWeight: "bold" }}>Resell</Button>
         </Link>
-        <TextField
+        {/* <TextField
           type="text"
           label="Search"
           margin="dense"
@@ -57,25 +57,29 @@ const Menu = withRouter(({ history }) => (
         />
         <Link to="/shops/NA">
           <Button style={isActive(history, "/shops/all")}>Search</Button>
-        </Link>
+        </Link> */}
       </div>
       <div style={{ 'position': 'absolute', 'right': '10px' }}><span style={{ 'float': 'right' }}>
         {
           !auth.isAuthenticated() && (<span>
             <Link to="/cart">
-              <Button style={isActive(history, "/cart")}>
-                Cart
+              <Button style=
+              {{ ...isActive(history, "/cart"), fontSize: "25px", important: "true", fontWeight: "bold" }}>Cart
+
                 <Badge color="secondary" invisible={false} badgeContent={cart.itemTotal()} style={{ 'marginLeft': '7px' }}>
                   <CartIcon />
                 </Badge>
               </Button>
             </Link>
             <Link to="/signup">
-              <Button style={isActive(history, "/signup")}>Sign up
+              <Button style=
+              {{ ...isActive(history, "/signup"), fontSize: "25px", important: "true" , fontWeight: "bold"}}>Sign up
               </Button>
             </Link>
             <Link to="/signin">
-              <Button style={isActive(history, "/signin")}>Sign In
+              <Button style=
+              {{ ...isActive(history, "/signin"), fontSize: "25px", important: "true", fontWeight: "bold" }}>Sign In
+
               </Button>
             </Link>
           </span>)
